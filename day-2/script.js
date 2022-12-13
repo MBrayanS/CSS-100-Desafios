00c86424dd34285bd4bc271090c2a8fe33018bdc
MBrayan25 <micael.brayan.s@gmail.com>
2022-12-13 12:17:18 -0300

    Removi a pasta day-2

--- day-2/script.js
+++
@@ -1,5 +0,0 @@
-$('.menu').bind('click',function(){
-	$('.frame').toggleClass('active-animation')
-	$('.frame').removeClass('no-animation')
-	$(this).children('div').removeClass('no-animation')
-}) 
